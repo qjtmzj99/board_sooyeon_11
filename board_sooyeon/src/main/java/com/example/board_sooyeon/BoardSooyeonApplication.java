69d238eee68a38103a82fa506cb21c16b851ce3e
Sooyeon <qjtmzj99@gmail.com>
2024-08-10 19:21:11 +0900

    생성

---
+++ board_sooyeon/src/main/java/com/example/board_sooyeon/BoardSooyeonApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_sooyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSooyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSooyeonApplication.class, args);
+	}
+
+}
---
+++ board_sooyeon/src/test/java/com/example/board_sooyeon/BoardSooyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_sooyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSooyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
